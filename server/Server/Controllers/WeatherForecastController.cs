2968abd506fc7ab1f8628b41ef087c33ef83149f
Nguyen Le Vinh <vikvenus199x@gmail.com>
2025-01-11 14:18:36 +0700

    [MBS-0] downgrade .net 8 to .net 6

--- server/Server/Controllers/WeatherForecastController.cs
+++ server/Server/Controllers/WeatherForecastController.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 
-namespace MobileApp.Controllers
+namespace Server.Controllers
 {
     [ApiController]
     [Route("[controller]")]
@@ -23,7 +23,7 @@ namespace MobileApp.Controllers
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
--- server/Server/WeatherForecast.cs
+++ server/Server/WeatherForecast.cs
@@ -1,8 +1,8 @@
-namespace MobileApp
+namespace Server
 {
     public class WeatherForecast
     {
-        public DateOnly Date { get; set; }
+        public DateTime Date { get; set; }
 
         public int TemperatureC { get; set; }
 
